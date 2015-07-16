338af90918155818bc1ecd0fedcae08a54f9afcf
Saul Giordani <saulgiordani@gmail.com>
2015-07-17 00:17:16 +0200

    autor

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,6 @@ router.get('/', function(req, res) {
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
-router.get('/quizes/creditos', quizController.creditos);
+router.get('creditos', quizController.creditos);
 
 module.exports = router;
