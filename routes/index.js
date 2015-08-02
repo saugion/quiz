06ca0ed7ba3bef9012b85d509ca4c63435ec3907
Saul Giordani <saulgiordani@gmail.com>
2015-08-02 19:16:57 +0200

    despliegue db

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,11 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('/index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz' });
 });
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
 
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
