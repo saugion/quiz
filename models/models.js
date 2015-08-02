f5513b663515dd6770fc9d1adf2450095090899d
Saul Giordani <saulgiordani@gmail.com>
2015-08-02 17:58:32 +0200

    despliegue db

--- models/models.js
+++ models/models.js
@@ -34,13 +34,13 @@ exports.Quiz = Quiz;
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
 	// success(...) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success(function (count) {
+	Quiz.count().then(function (count) {
 		if(count === 0) {
 			// la tabla se inicializa solo se esta vacia
 			Quiz.create({	pregunta: 'Capital de Italia',
 						 	respuesta: 'Roma'
 						})
-			.success(function(){
+			.then(function(){
 				console.log('Base de datos inicializada')
 			});
 		}
