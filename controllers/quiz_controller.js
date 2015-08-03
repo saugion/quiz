a4b688306ea664124ab21089191da7f54b6ade67
Saul Giordani <saulgiordani@gmail.com>
2015-08-03 21:31:07 +0200

    editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -52,3 +52,24 @@ exports.index=function(req,res){
 		next(error);
 	});
 };
+
+// GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build( // crea objeto quiz
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+		);
+
+	res.render('quizes/new', {quiz: quiz});
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	// guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function() {
+		res.redirect('/quizes');
+	});	// redirection HTTP (URL relativo) lista de preguntas
+};
+
+
