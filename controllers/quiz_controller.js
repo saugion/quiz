7bc3989a6f7809c9ca1522ceafe3965ca4d4e440
Saul Giordani <saulgiordani@gmail.com>
2015-08-04 23:08:33 +0200

    modulo 8 eje

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -57,7 +57,7 @@ exports.index=function(req,res){
 // GET /quizes/new
 exports.new = function(req, res) {
 	var quiz = models.Quiz.build( // crea objeto quiz
-		{pregunta: "Pregunta", respuesta: "Respuesta"}
+		{pregunta: "Pregunta", respuesta: "Respuesta", tema: "Tema"}
 		);
 
 	res.render('quizes/new', {quiz: quiz, errors: []});
@@ -76,7 +76,7 @@ exports.create = function(req, res) {
 				res.render('quizes/new', {quiz: quiz, errors: err.errors});
 			} else {
 				quiz
-				.save({fields: ["pregunta", "respuesta"]})
+				.save({fields: ["pregunta", "respuesta", "tema"]})
 				.then(function() { res.redirect('/quizes')});
 			}	//	res.redirect
 		}
--- models/quiz.js
+++ models/quiz.js
@@ -12,7 +12,7 @@ module.exports = function(sequelize, DataTypes) {
 			},
 				tema:  		{
         		type:       DataTypes.STRING,
-        		validate:   { notEmpty: {msg: "->Falta tema"}}
+        		validate:   { notEmpty: {msg: "-> Falta tema"}}
         	}
     	}
 	);
